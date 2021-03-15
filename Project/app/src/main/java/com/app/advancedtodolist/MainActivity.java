5ead4bed6942c8c8c0ea99f028b20e5d652f7317
Janicska Balázs <gm1rco@inf.elte.hu>
2021-03-15 21:05:44 +0100

    - Main activity (kb kész)     - cím, köszönés, input, gomb     - gomb átvisz másik activitybe, eltárolja a nevet     - háttér és egyéb beállítások - Today activity (elkezdve) - további activitik tervezése

--- Project/app/src/main/java/com/app/advancedtodolist/MainActivity.java
+++ Project/app/src/main/java/com/app/advancedtodolist/MainActivity.java
@@ -2,7 +2,15 @@ package com.app.advancedtodolist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.SharedPreferences;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+
+        TextInputEditText input = (TextInputEditText) findViewById(R.id.input);
+        Button next = (Button) findViewById(R.id.buttonNext);
+
+        next.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                editor.putString("name", input.getText().toString());
+                editor.commit();
+
+                startActivity(new Intent(MainActivity.this, today.class));
+            }
+        });
     }
 }
