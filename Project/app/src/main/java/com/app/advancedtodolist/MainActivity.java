67cc246cdb0fe6878728199b7f6cb27f63cf4d45
Deme Szabolcs <jdfwv4@inf.elte.hu>
2021-03-16 15:50:34 +0100

    A beállítások activity létrehoztam, a vázlatát megcsináltam. Az elmentett nevet már meg lehet változtatni. Az input mezőre pár ellenőrzést szeretnék még tenni, hogy ne lehessen üres nevet elmenteni, illetve több beállítási lehetőségen is gondolkodok.

--- Project/app/src/main/java/com/app/advancedtodolist/MainActivity.java
+++ Project/app/src/main/java/com/app/advancedtodolist/MainActivity.java
@@ -18,21 +18,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
         SharedPreferences sharedPreferences = getSharedPreferences("MyPref", MODE_PRIVATE);
+        sharedPreferences.edit().clear().commit();
         SharedPreferences.Editor editor = sharedPreferences.edit();
-
         TextInputEditText input = (TextInputEditText) findViewById(R.id.input);
         Button next = (Button) findViewById(R.id.buttonNext);
-
         next.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 editor.putString("name", input.getText().toString());
                 editor.commit();
-
                 startActivity(new Intent(MainActivity.this, today.class));
             }
         });
