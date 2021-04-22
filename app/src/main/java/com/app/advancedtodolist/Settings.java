ac7e53bda567b8f22257f5665a845edbb9f4009f
Janicska Balázs <gm1rco@inf.elte.hu>
2021-04-22 21:58:05 +0200

    .

---
+++ app/src/main/java/com/app/advancedtodolist/Settings.java
@@ -0,0 +1,47 @@
+package com.app.advancedtodolist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+public class Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        TextInputEditText nameInput = (TextInputEditText) findViewById(R.id.nameInput);
+        SharedPreferences sharedPreferences = this.getSharedPreferences("MyPref", MODE_PRIVATE);
+        String yourname=sharedPreferences.getString("name", "");
+
+        nameInput.setText(yourname);
+        Button saveBtn = (Button) findViewById(R.id.saveBtn);
+        saveBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String newname=nameInput.getText().toString();
+                if(newname.trim().length()!=0)
+                {
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putString("name", newname);
+                    editor.commit();
+                    startActivity(new Intent(Settings.this, Today.class));
+                }
+                else
+                {
+                    nameInput.setError("You need to enter a name");
+                }
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/app/advancedtodolist/Today.java
@@ -0,0 +1,86 @@
+package com.app.advancedtodolist;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.view.View.OnClickListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Today extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today);
+
+        ListView lv = (ListView) findViewById(R.id.listview);
+        TextView date = (TextView) findViewById(R.id.date);
+        ImageView settingsImage = (ImageView) findViewById(R.id.settingsImg);
+        ImageView addImage = (ImageView) findViewById(R.id.addImg);
+        ImageView calendarImage = (ImageView) findViewById(R.id.calendarImg);
+
+
+        settingsImage.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+                startActivity(new Intent(Today.this, Settings.class));
+            }
+        });
+
+        addImage.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+                startActivity(new Intent(Today.this, AddPopUp.class));
+            }
+        });
+
+        calendarImage.setOnClickListener(new OnClickListener() {
+
+            public void onClick(View view) {
+                startActivity(new Intent(Today.this, Calendar.class));
+            }
+        });
+
+        ArrayList<Item> items = new ArrayList<Item>();
+
+        date.setText(getDate(System.currentTimeMillis()));
+
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_list_item_1,
+                 toStringArray(items));
+
+        lv.setAdapter(arrayAdapter);
+
+
+    }
+
+    private String getDate(long millis){
+        return DateFormat.format("yyyy.MM.dd, hh:mm",millis).toString();
+    }
+
+    private ArrayList<String> toStringArray(ArrayList<Item> it){
+        ArrayList<String> items = new ArrayList<>();
+        for(Item i : it){
+            items.add(i.toString());
+        }
+        return items;
+    }
+}
